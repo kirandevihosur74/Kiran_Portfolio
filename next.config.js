52b9ae6498a4929c9c9ac880193c1a93798b7dcd
Kiran Devihosur <kirandevihosur74@gmail.com>
2025-06-25 20:03:39 -0700

    Add changes

--- next.config.js
+++ next.config.js
@@ -10,21 +10,6 @@ const withMDX = require('@next/mdx')({
 const nextConfig = {
   output: 'export',
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    mdxRs: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-    ],
-  },
 }
 
 module.exports = withMDX(nextConfig)
